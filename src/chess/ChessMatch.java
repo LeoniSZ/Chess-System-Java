300273d76a339708372bb5a6109ac7471212cb04
Elioenai Roberto <elioenaipro@gmail.com>
2023-07-11 17:02:23 -0300

    Placing pieces on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,15 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.*;
 
 public class ChessMatch {
 	private Board board;
 
 	public ChessMatch() {
 		board = new Board(8,8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces (){
@@ -20,5 +23,26 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void initialSetup() {
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 4));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 3));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 2));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 5));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 1));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 6));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 7));
+		board.placePiece(new Pawn(board, Color.WHITE), new Position(6, 0));
+	
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+		board.placePiece(new Queen(board, Color.WHITE), new Position(7, 3));
+		board.placePiece(new Bishop(board, Color.WHITE), new Position(7, 2));
+		board.placePiece(new Bishop(board, Color.WHITE), new Position(7, 5));
+		board.placePiece(new Knight(board, Color.WHITE), new Position(7, 1));
+		board.placePiece(new Knight(board, Color.WHITE), new Position(7, 6));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 7));
+		board.placePiece(new Rook(board, Color.WHITE), new Position(7, 0));
+		
+	
+	}
 	
 }
