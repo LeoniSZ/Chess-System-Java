30bcdb669926c47e010a4ffa886aef1aa4ec10c8
Elioenai Roberto <elioenaipro@gmail.com>
2023-07-13 18:40:40 -0300

    Moving pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,12 +1,10 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.BoardException;
+import boardgame.Piece;
 import boardgame.Position;
-import chess.pieces.Bishop;
 import chess.pieces.King;
-import chess.pieces.Knight;
-import chess.pieces.Pawn;
-import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -50,4 +48,27 @@ public class ChessMatch {
 	
 	}
 	
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		
+		validateSourcePosition(source);
+		
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece)capturedPiece;
+	}
+	
+	private Piece makeMove(Position source, Position target) {
+		
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+	}
+
+	private void validateSourcePosition(Position position) {
+		if(!board.thereIsAPiece(position)) {
+			throw new ChessException("Position not a piece on source position.");
+		}
+	}
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -5,6 +5,8 @@ import boardgame.Position;
 public class ChessPosition {
 	private char column;
 	private int row;
+	
+	
 	public ChessPosition(char column, int row) {
 		if(column < 'a' ||column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values are a1 to h8.");
